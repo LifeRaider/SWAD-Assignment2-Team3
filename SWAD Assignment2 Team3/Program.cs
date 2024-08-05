4aaa07dc781fbdb682e0fef6c9234d76915b0c22
LoocasToh <s10257164@connect.np.edu.sg>
2024-08-05 23:26:32 +0800

    classes commit

--- SWAD Assignment2 Team3/Program.cs
+++ SWAD Assignment2 Team3/Program.cs
@@ -1,17 +1,1 @@
-﻿using System;
-
-namespace CarRental
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var iCarSystem = new iCarSystem();
-            var controller = new CTL_ReturnVehicle(iCarSystem);
-            var ui = new UI_ReturnVehicle(controller);
-            var renter = new Renter("Alice", "R123456");
-
-            renter.SelectReturnVehicle(ui);
-        }
-    }
-}
+﻿
